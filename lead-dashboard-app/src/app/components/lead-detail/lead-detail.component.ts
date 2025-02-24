6e2ce018bcdc873d1590b421673bc630d336ce9e
SensibleSoftware805 <matt@sensiblesoftwaresolutions.com>
2025-02-24 10:33:31 -0800

    Modified lead detail component display

--- lead-dashboard-app/src/app/components/lead-detail/lead-detail.component.ts
+++ lead-dashboard-app/src/app/components/lead-detail/lead-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Lead } from '../../models/lead';
+import dateFormat from 'dateformat';
 
 /**
  * Component that displays the details of a lead.
@@ -18,4 +19,13 @@ export class LeadDetailComponent {
   @Input()
   lead: Lead | null = null;
 
+  /**
+     * Formats a date into a string.
+     * @param date The date to format.
+     */
+    formatDate(date: Date | undefined) : string {
+      return date
+        ? dateFormat(date, 'mm/dd/yyyy hh:MM TT')
+        : '';
+    }
 }
